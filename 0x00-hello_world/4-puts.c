c7d08d997041e047cc63e3188cba5b807e73c3e7
Mary-Afrika <marygahar@gmail.com>
2023-02-16 10:36:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - printing a line that puts function
- * Return: 0
- */
+*main - printing a line that puts function
+*Return: 0 (Success)
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingualpuzzle");
