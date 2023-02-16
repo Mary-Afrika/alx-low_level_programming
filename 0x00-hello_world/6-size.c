0b4a584893b476aa47f9fd0e7482dd0435164ca6
Mary-Afrika <marygahar@gmail.com>
2023-02-16 12:46:36 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,13 @@
  * Returns 0 (Success)
  */
 
-int main(void)
+int main(void) - Sizes of input
 {
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
