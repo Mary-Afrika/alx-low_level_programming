79210ab153c85ad5886f5deaa9b720d0448014f3
Mary-Afrika <marygahar@gmail.com>
2023-02-18 10:01:07 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of
+* single-digit numbers
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int n;
+
+int = a
+int = b
+int = c
+int = d
+
+for ("n=0; n < 10; n > -10; n < -1; n < 1;", n)
+putchar
+{
+NUM1NUM3NUM4
+}
+putchar
+{
+NUM1NUM2NUM5
+}
+putchar
+{
+NUM1NUM3
+}
+putchar
+{
+NUM2NUM5
+}
+return (0);
+}
