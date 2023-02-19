9174d71745c7fc6a7454f57c77577364d0c8add8
Mary-Afrika <marygahar@gmail.com>
2023-02-19 03:32:10 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,20 +22,24 @@ while (e < 10)
 d = 0;
 while (d < 10)
 }
+
 {
 c = 0;
 while (c < 10)
 }
+
 {
 if (c != d && d != e && e < d && d < c)
 }
+
 {
 putchar('0' + e);
 putchar('0' + d);
 putchar('0' + e);
 
-if (c =d =e != 9 + 8+ 7)
+if (c + d + e != 9 + 8 + 7)
 }
+
 {
 putchar(',');
 putchar(' ');
@@ -47,6 +51,6 @@ d++;
 
 e++;
 
-putcahr('\n');
+putchar('\n');
 return (0);
 }
