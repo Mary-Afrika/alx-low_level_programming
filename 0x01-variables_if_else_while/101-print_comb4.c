2063232181415c2ecc3c72987e5ba34143cbaeab
Mary-Afrika <marygahar@gmail.com>
2023-02-19 12:28:48 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,13 +16,13 @@ int c;
 int d;
 int e = 0;
 
-while (e < 10)
+while (e <= 9 )
 
 d = 0;
-while (d < 10)
+while (d <= 9)
 
 c = 0;
-while (c < 10)
+while (c <= 9)
 
 if (c != d && d != e && e < d && d < c)
 
