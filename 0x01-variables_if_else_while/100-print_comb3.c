01e256f1a791d2ff14667012f92478b5798f601b
Mary-Afrika <marygahar@gmail.com>
2023-02-20 05:21:11 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,32 @@ int main(void)
 int ones = '0';
 int tens = 0;
 
-for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+for (tens = '0';
+tens <= '9';
+tens++);
+
+/* prints tens digit*/
 {
-for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
+for (ones = '0';
+ones <= '9';
+ones++);
+
+/*prints ones digit*/
 }
 
 {
-if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+if (!((ones == tens)
+|| (tens > ones)));
+
+/*eliminates repitition*/
 }
 
 {
 putchar(tens);
 putchar(ones);
-if (!(ones == '9' && tens == '8'))/*adds comma and spaces*/
+if (!(ones == '9'
+&& tens == '8'));
+/*adds comma and spaces*/
 }
 
 {
