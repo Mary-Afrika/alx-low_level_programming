df1183dbe1b045491035e8049b21340dc4dbfb15
Mary-Afrika <marygahar@gmail.com>
2023-02-20 05:09:17 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,17 @@ for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 {
 for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
 }
+
 {
 if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 }
+
 {
 putchar(tens);
 putchar(ones);
 if (!(ones == '9' && tens == '8'))/*adds comma and spaces*/
 }
+
 {
 putchar(',');
 putchar(' ');
