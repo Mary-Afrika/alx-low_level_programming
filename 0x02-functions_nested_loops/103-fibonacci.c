8bb11cb96f48e7eb2c090c9c40bfb1e28613e657
Mary-Afrika <marygahar@gmail.com>
2023-02-21 15:22:17 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,13 +12,14 @@ int main(void)
 
 int i = 0;
 long j = i, k = 2, sum = k;
-{
+
 while (k + j < 4000000)
+{
 k =+ j;
 if (k % 2 == 0)
 sum += k;
 j = k - j;
-i++;
+++1;
 }
 printf("%ld\n", sum);
 return (0);
