0d9c1e646314eebb4a2aee032679c36f84184ddc
Mary-Afrika <marygahar@gmail.com>
2023-02-23 05:24:22 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
 * more_numbers - prints numbers between 0 to 14
 * 10 times.
-* 
+*
 * Return: no return.
 */
 void more_numbers(void)
@@ -12,7 +12,8 @@ int i, ch;
 for (i = 0; i < 10; i++)
 {
 for (ch = 0; ch < 15; ch++)
-{if (ch >= 10)
+{
+if (ch >= 10)
 _putchar((ch / 10) + 48);
 _putchar((ch % 10) + 48);
 }
